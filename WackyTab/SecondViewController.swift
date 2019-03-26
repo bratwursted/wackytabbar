2c78a7228b61b30c993b049ea44539a84b358272
Joe Bramhall <joe.bramhall@nytimes.com>
2019-03-26 11:43:09 -0400

    Initial Commit

---
+++ WackyTab/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WackyTab
+//
+//  Created by Bramhall, Joe on 3/26/19.
+//  Copyright © 2019 Bramhall, Joe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
