bfe515614fd766a056dd96a695372db2d79c336b
Joe Bramhall <joe.bramhall@nytimes.com>
2019-04-09 14:50:02 -0400

    simple way to display items in the table view

--- WackyTab/FirstViewController.swift
+++ WackyTab/FirstViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class FirstViewController: UITableViewController {
 
+    static let CellIdentifier = "Cell"
+
+    static let Formatter = ISO8601DateFormatter()
+
+    var items = [Date]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Top Stories"
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewCell(_:)))
+        navigationItem.rightBarButtonItem = addButton
     }
 
-
+    @objc func addNewCell(_ sender: Any) {
+        items.append(Date())
+        tableView.reloadData()
+    }
 }
 
+// MARK: - Table view data source
+extension FirstViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: FirstViewController.CellIdentifier) {
+            configureCell(cell, forItemAt: indexPath)
+            return cell
+        }
+        else {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: FirstViewController.CellIdentifier)
+            configureCell(cell, forItemAt: indexPath)
+            return cell
+        }
+    }
+
+    private func configureCell(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let cellDate = items[indexPath.row]
+        cell.textLabel?.text = FirstViewController.Formatter.string(from: cellDate)
+    }
+}
